f2b14c73a7134f504b4fba29b27861fc3b25b404
Md.Sami Ul Haque <samiulh4@gmail.com>
2023-04-07 22:44:09 +0600

    new project
    project setup

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,13 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-    return (
-        <div>
-            <h1>My Todo</h1>
-            <Todo text="Learn React js"/>
-            <Todo text="Learn Vue Js"/>
-            <Todo text="Learn Angular Js"/>
-        </div>);
+    return <div>Hello</div>;
 }
 
 export default App;
